c08a6ae0b4f3f26680a0b7e77fefbb892c09968e
Nadi Campbell <nadicampbell97@hotmail.com>
2023-05-29 19:02:24 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,8 @@
+def food_order(food,drink,cost):
+    print("your food is: " + food + ". your drink is: " + drink + ". your total cost is: " + cost)
+food_order("Burger", "Cola", "10")
+
+def my_add(num1, num2):
+    result = num1 + num2
+    print(result)
+my_add(5,5)
